527c2fb26f8f198360ef08e08fe9b706bf8bc8e1
Afathy-99 <abdelrahmn.abdelsalam@dafagate.com>
2025-12-05 07:23:41 +0200

    publish lang files

---
+++ lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+
+return [
+    'cannot_complete_task_some_dependecies_are_not_completed_yet' => 'Can not Complete This Task, Some dependencies are not completed yet',
+    'validation_failed' => 'Validation Failed'
+];
